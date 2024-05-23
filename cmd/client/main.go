32418dd013841703a6e3efb0413ba11eb28052f1
Mike Gardner <mjgardner@abitofhelp.com>
2024-05-22 22:14:26 -0700

    WIP: Still wrangling the generation of files via bazel.

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,32 +1,32 @@
 package main
 
-import (
-	"context"
-	"log"
-	"net/http"
-
-	"connectrpc.com/connect"
-	// Use the following imports when generating from proto files using 'buf generate'.
-	// helloworldv1 "github.com/abitofhelp/bzlmod/internal/gen/abitofhelp/helloworld/v1"
-	// "github.com/abitofhelp/bzlmod/internal/gen/abitofhelp/helloworld/v1/helloworldv1connect"
-
-	// Use the following imports when generating from proto files using Bazel build.
-	helloworldv1 "github.com/abitofhelp/bzlmod/bazel-bin/proto/abitofhelp/helloworld/v1/abitofhelp_helloworld_v1_go_proto_/github.com/abitofhelp/bzlmod/proto/abitofhelp/helloworld/v1"
-	// FIXME: "github.com/abitofhelp/bzlmod/bazel-bin/proto/abitofhelp/helloworld/v1/abitofhelp_helloworld_v1_go_proto_/github.com/abitofhelp/bzlmod/proto/abitofhelp/helloworld/v1/helloworldconnect"
-)
-
+// import (
+//
+//	"context"
+//	"log"
+//	"net/http"
+//
+//	"connectrpc.com/connect"
+//	// Use the following imports when generating from proto files using 'buf generate'.
+//	// helloworldv1 "github.com/abitofhelp/bzlmod/internal/gen/abitofhelp/helloworld/v1"
+//	// "github.com/abitofhelp/bzlmod/internal/gen/abitofhelp/helloworld/v1/helloworldv1connect"
+//	// Use the following imports when generating from proto files using Bazel build.
+//	//helloworldv1 "github.com/abitofhelp/bzlmod/bazel-bin/proto/abitofhelp/helloworld/v1/abitofhelp_helloworld_v1_go_proto_/github.com/abitofhelp/bzlmod/proto/abitofhelp/helloworld/v1"
+//	// FIXME: "github.com/abitofhelp/bzlmod/bazel-bin/proto/abitofhelp/helloworld/v1/abitofhelp_helloworld_v1_go_proto_/github.com/abitofhelp/bzlmod/proto/abitofhelp/helloworld/v1/helloworldconnect"
+//
+// )
 func main() {
-	client := helloworldv1connect.NewGreeterServiceClient(
-		http.DefaultClient,
-		"http://localhost:8080",
-	)
-	res, err := client.SayHello(
-		context.Background(),
-		connect.NewRequest(&helloworldv1.SayHelloRequest{Name: "Jane"}),
-	)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	log.Println(res.Msg.Message)
+	//	client := helloworldv1connect.NewGreeterServiceClient(
+	//		http.DefaultClient,
+	//		"http://localhost:8080",
+	//	)
+	//	res, err := client.SayHello(
+	//		context.Background(),
+	//		connect.NewRequest(&helloworldv1.SayHelloRequest{Name: "Jane"}),
+	//	)
+	//	if err != nil {
+	//		log.Println(err)
+	//		return
+	//	}
+	//	log.Println(res.Msg.Message)
 }
--- go.mod
+++ go.mod
@@ -2,12 +2,20 @@ module github.com/abitofhelp/bzlmod
 
 go 1.22
 
+// go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+// go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+// go install connectrpc.com/connect/cmd/protoc-gen-connect-go@latest
+
 require (
 	connectrpc.com/connect v1.16.2
 	connectrpc.com/grpchealth v1.3.0
 	connectrpc.com/grpcreflect v1.2.0
 	golang.org/x/net v0.25.0
-	google.golang.org/protobuf v1.33.0
+	google.golang.org/protobuf v1.34.1
 )
 
-require golang.org/x/text v0.15.0 // indirect
+require (
+	golang.org/x/text v0.15.0 // indirect
+	//google.golang.org/protobuf/cmd/protoc-gen-go v1.3.0 // indirect
+    //google.golang.org/grpc/cmd/protoc-gen-go-grpc v0.0.0 // indirect
+)
